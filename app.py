6f8e833b87150a5bb9258ad688ac5ecc43f32f35
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-01-02 16:20:49 +0100

    comprisoin en sterker secret key

--- app.py
+++ app.py
@@ -5,10 +5,18 @@ from string import ascii_uppercase,ascii_lowercase
 
 def random_letters(wachtwoord_items:list) -> list:
   for _ in range(randint(1,99)):
+    if randint(1,2) == 1:
+      wachtwoord_items.append(choice(ascii_uppercase))
       if randint(1,2) == 1:
-          wachtwoord_items.append(choice(ascii_uppercase))
-      elif randint(1,2) == 2:
-          wachtwoord_items.append(choice(ascii_lowercase))
+        wachtwoord_items.append(choice(ascii_uppercase))
+      if randint(1,2) == 2:
+        wachtwoord_items.append(choice(ascii_lowercase))
+    elif randint(1,2) == 2:
+      wachtwoord_items.append(choice(ascii_lowercase))
+      if randint(1,2) == 1:
+        wachtwoord_items.append(choice(ascii_uppercase))
+      if randint(1,2) == 2:
+        wachtwoord_items.append(choice(ascii_lowercase))
   return wachtwoord_items
 
 def sterk_wachtwoord() -> str:
@@ -39,37 +47,37 @@ rooms = {}
 
 def generate_unique_code(length:int) -> str:
   while True:
-      code = ""
-      for _ in range(length):
-          code += choice(ascii_uppercase)
-      if code not in rooms:
-          break
+    code = ""
+    for _ in range(length):
+      code += choice(ascii_uppercase)
+    if code not in rooms:
+      break
   return code
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
   session.clear()
   if request.method == "POST":
-      naam = request.form.get("naam")
-      wachtwoord = request.form.get("wachtwoord")
-      # zoek op in database
-      # check wachtwoord
-      if naam == "test" and wachtwoord == "test":
-          session["naam"] = naam
-          print(f"DEBUG: gebruiksnaam: {naam}, wachtwoord: {wachtwoord}")
-          return redirect(url_for("home"))
+    naam = request.form.get("naam")
+    wachtwoord = request.form.get("wachtwoord")
+    # zoek op in database
+    # check wachtwoord
+    if naam == "test" and wachtwoord == "test":
+      session["naam"] = naam
+      print(f"DEBUG: gebruiksnaam: {naam}, wachtwoord: {wachtwoord}")
+      return redirect(url_for("home"))
   return render_template("login.html")
 
 @app.route("/create_acount", methods=["POST","GET"])
 def create_acount():
   session.clear()
   if request.method == "POST":
-      naam = request.form.get("gebruikersnaam")
-      wachtwooord = request.form.get("wachtwoord")
-      email = request.form.get("email")
-      # verzend verfie email
-      # als op de link in de email wordt geclickt dan:
-      # voeg toe aan database
+    naam = request.form.get("gebruikersnaam")
+    wachtwooord = request.form.get("wachtwoord")
+    email = request.form.get("email")
+    # verzend verfie email
+    # als op de link in de email wordt geclickt dan:
+    # voeg toe aan database
   return render_template('create_acount.html')
 
 @app.route("/")
@@ -79,30 +87,33 @@ def redrict():
 @app.route("/home", methods=["POST", "GET"])
 def home():
  if request.method == "POST":
+    try:
+      naam = session["naam"]
+    except:
       naam = request.form.get("naam")
-      code = request.form.get("code")
-      room_id = request.form.get("room_id")
-      join = request.form.get("join", False)
-      create = request.form.get("create", False)
-      if not naam:
-          return render_template("home.html", error="Please enter a naam.", code=code, name=naam)
-      if join != False and not code:
-          return render_template("home.html", error="Please enter a room code.", code=code, name=naam)
-      room = code
-      if create != False:
-          if room_id == "":
-              room = generate_unique_code(4)
-          else:
-              room = room_id
-          if room not in rooms:
-              rooms[room] = {"members": 0, "messages": []}
-          elif room in rooms:
-              return render_template("home.html", error="Room already exist.", code=code, name=naam)
-      elif code not in rooms:
-          return render_template("home.html", error="Room does not exist.", code=code, name=naam)
-      session["room"] = room
-      session["naam"] = naam
-      return redirect(url_for("room"))
+    code = request.form.get("code")
+    room_id = request.form.get("room_id")
+    join = request.form.get("join", False)
+    create = request.form.get("create", False)
+    if not naam:
+      return render_template("home.html", error="Please enter a naam.", code=code, name=naam)
+    if join != False and not code:
+      return render_template("home.html", error="Please enter a room code.", code=code, name=naam)
+    room = code
+    if create != False:
+      if room_id == "":
+        room = generate_unique_code(4)
+      else:
+        room = room_id
+      if room not in rooms:
+        rooms[room] = {"members": 0, "messages": []}
+      elif room in rooms:
+        return render_template("home.html", error="Room already exist.", code=code, name=naam)
+    elif code not in rooms:
+      return render_template("home.html", error="Room does not exist.", code=code, name=naam)
+    session["room"] = room
+    session["naam"] = naam
+    return redirect(url_for("room"))
  return render_template("home.html")
 
 @app.route("/doc")
@@ -113,17 +124,17 @@ def doc():
 def room():
   room = session.get("room")
   if room is None or session.get("naam") is None or room not in rooms:
-      return redirect(url_for("home"))
+    return redirect(url_for("home"))
   return render_template("room.html", code=room, messages=rooms[room]["messages"])
 
 @socketio.on("message")
 def message(data):
   room = session.get("room")
   if room not in rooms:
-      return
+    return
   content = {
-      "naam": session.get("naam"),
-      "message": data["data"]}
+    "naam": session.get("naam"),
+    "message": data["data"]}
   send(content, to=room)
   rooms[room]["messages"].append(content)
 
@@ -132,10 +143,10 @@ def connect(auth):
   room = session.get("room")
   naam = session.get("naam")
   if not room or not naam:
-      return
+    return
   if room not in rooms:
-      leave_room(room)
-      return
+    leave_room(room)
+    return
   join_room(room)
   send({"naam": naam, "message": "has entered the room"}, to=room)
   rooms[room]["members"] += 1
@@ -146,9 +157,9 @@ def disconnect():
   naam = session.get("naam")
   leave_room(room)
   if room in rooms:
-      rooms[room]["members"] -= 1
-      if rooms[room]["members"] <= 0:
-          del rooms[room]
+    rooms[room]["members"] -= 1
+    if rooms[room]["members"] <= 0:
+      del rooms[room]
   send({"naam": naam, "message": "has left the room"}, to=room)
 
 if __name__ == "__main__":
