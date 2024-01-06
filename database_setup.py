3d8f823f3d5c719ff4ee4236112d839d564f23f3
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-01-06 17:47:58 +0100

    insert sql command bug

--- database_setup.py
+++ database_setup.py
@@ -3,7 +3,7 @@ import sqlite3
 conn = sqlite3.connect('database.db')
 print('Connected to database succesfully')
 
-conn.execute('CREATE TABLE gebruikers (id INTEGER PRIMARY KEY AUTOINCREMENT, naam text, wachtwoord text)')
+conn.execute('INSERT into gebruikers (id,naam,wachtwoord) values (1,"test","test")')
 
 print('sql code succesful')
 conn.close()
