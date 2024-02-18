92739e51b9d21355ba39a90793dbd2c5a88f0de7
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-02-18 17:43:27 +0100

    add types to database_setup.py

--- database_setup.py
+++ database_setup.py
@@ -1,8 +1,8 @@
 import sqlite3
 
-conn = sqlite3.connect('dev.db')
+conn:sqlite3.Connection = sqlite3.connect('dev.db')
 
-cursor = conn.cursor()
+cursor:sqlite3.Cursor = conn.cursor()
 
 cursor.execute('CREATE TABLE gebruikers (id INTEGER AUTO INCREMENT PRIMARY KEY, naam TEXT, wachtwoord TEXT)')
 cursor.execute('CREATE TABLE emails (id INTEGER AUTO INCREMENT PRIMARY KEY, email TEXT)')
